6d6b57e994f0f611a2fd10341cc880087c403949
Jase Seeley <jcseeley31590@yahoo.com>
2022-02-24 08:55:13 -0800

    WIP: writing sifting loops.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
 using System;
 using System.Collections.Generic;
+
+public class Program
+{
+  public static List<int> intList = new List<int> {};
+
+  public static void Main()
+  {
+    Console.WriteLine("Please enter a number:");
+    int response = int.Parse(Console.ReadLine());
+    
+    for(int i = 2; i < response; i++)
+    {
+      if (i < response)
+      {
+        intList.Add(i);
+      }
+    }
+    for(int i = 0; i < intList.Count(); i++)
+    {
+      int prime = 2;
+      if(intList[i] % prime == 0)
+      {
+        intList.Remove(intList[i]);
+      }
+    }
+    foreach(int num in intList)
+    {
+      Console.WriteLine(num);
+    }
+  }
+}
